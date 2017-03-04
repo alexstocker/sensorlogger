8843d21329fb6d4c9fb75ddfda8ae691f4c58bf4
Alexander Stocker <alex@stocker.info>
2017-03-04 11:54:19 +0100

    Updated API Controller

--- controller/apisensorloggercontroller.php
+++ controller/apisensorloggercontroller.php
@@ -1,18 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: elex
- * Date: 19.02.17
- * Time: 15:28
- */
 
 namespace OCA\SensorLogger\Controller;
 
+use OCA\SensorLogger\SensorDevices;
 use OCP\AppFramework\ApiController;
 use OCP\IConfig;
 use OCP\IDBConnection;
 use OCP\IRequest;
 
+/**
+ * Class ApiSensorLoggerController
+ *
+ * @package OCA\SensorLogger\Controller
+ */
 class ApiSensorLoggerController extends ApiController {
 
 	private $userId;
@@ -37,13 +37,6 @@ class ApiSensorLoggerController extends ApiController {
 	}
 
 	/**
-	 * @CORS
-	 */
-	public function index() {
-
-	}
-
-	/**
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 * @CORS
@@ -52,6 +45,10 @@ class ApiSensorLoggerController extends ApiController {
 		$this->insertLog($this->request->getParams());
 	}
 
+	/**
+	 * @param array $array
+	 * @return bool
+	 */
 	protected function insertLog($array){
 		$registered = $this->checkRegisteredDevice($array);
 		if(isset($array['deviceId'])) {
@@ -68,7 +65,6 @@ class ApiSensorLoggerController extends ApiController {
 			$stmt = $this->db->prepare($sql);
 			$stmt->bindParam(1, $array['temperature']);
 			$stmt->bindParam(2, $array['humidity']);
-			//$stmt->bindParam(3, date('Y-m-d H:i:s'));
 			$stmt->bindParam(3, $array['date']);
 			$stmt->bindParam(4, $this->userId);
 			$stmt->bindParam(5, $deviceId);
@@ -84,14 +80,145 @@ class ApiSensorLoggerController extends ApiController {
 	 * @CORS
 	 */
 	public function registerDevice() {
+		//$this->checkRequestParams($this->request->getParams());
 		if(!$this->checkRegisteredDevice($this->request->getParams()) &&
 			$this->checkRegisteredDevice($this->request->getParams()) !== null) {
-			return $this->insertDevice($this->request->getParams());
+
+			$lastInsertId = $this->insertDevice($this->request->getParams());
+			if(is_int($lastInsertId)) {
+				$this->checkRequestParams($lastInsertId,$this->request->getParams());
+			}
+			//return $lastInsertId;
+			//return $this->insertDevice($this->request->getParams());
 		} else {
 			return true;
 		}
+		return false;
 	}
 
+	/**
+	 * @param array $array
+	 * @return int
+	 */
+	protected function insertDeviceType($array) {
+		$sql = 'INSERT INTO `*PREFIX*sensorlogger_device_types` (`user_id`,`device_typ_name`) VALUES(?,?)';
+		$stmt = $this->db->prepare($sql);
+		$stmt->bindParam(1, $this->userId);
+		$stmt->bindParam(2, $array['deviceType']);
+		if($stmt->execute()){
+			return (int)$this->db->lastInsertId();
+		}
+		return false;
+	}
+
+	/**
+	 * @param string $string
+	 * @return int
+	 */
+	protected function insertDeviceGroup($string) {
+		$sql = 'INSERT INTO `*PREFIX*sensorlogger_device_groups` (`user_id`,`device_group_name`) VALUES(?,?)';
+		$stmt = $this->db->prepare($sql);
+		$stmt->bindParam(1, $this->userId);
+		$stmt->bindParam(2, $string);
+		if($stmt->execute()){
+			return (int)$this->db->lastInsertId();
+		}
+		return false;
+	}
+
+	/**
+	 * @param array $array
+	 * @return int
+	 */
+	protected function insertDataTypes($array) {
+		$sql = 'INSERT INTO `*PREFIX*sensorlogger_data_types` (`user_id`,`description`,`type`,`short`) VALUES(?,?,?,?)';
+		$stmt = $this->db->prepare($sql);
+
+		if(isset($array['description'])) {
+			$description = $array['description'] ?: '';
+		} else {
+			$description = '';
+		}
+
+		if(isset($array['type'])) {
+			$type = $array['type'] ?: '';
+		} else {
+			$type = '';
+		}
+
+		if(isset($array['unit'])) {
+			$unit = $array['unit'] ?: '';
+		} else {
+			$unit = '';
+		}
+
+		$stmt->bindParam(1, $this->userId);
+		$stmt->bindParam(2, $description);
+		$stmt->bindParam(3, $type);
+		$stmt->bindParam(4, $unit);
+		if($stmt->execute()){
+			return (int)$this->db->lastInsertId();
+		}
+		return false;
+	}
+
+	/**
+	 * @param int $deviceId
+	 * @param array $params
+	 */
+	protected function checkRequestParams($deviceId,$params) {
+		if(isset($params['deviceType']) && !empty($params['deviceType'])) {
+			$deviceTypeId = $this->insertDeviceType($params);
+			if(is_int($deviceTypeId)) {
+				try {
+					SensorDevices::updateDevice($deviceId,'type',(string)$deviceTypeId,$this->db);
+				} catch (\Exception $e) {}
+			}
+		}
+		if(isset($params['deviceGroup']) && !empty($params['deviceGroup'])) {
+			$deviceGroupId = $this->insertDeviceGroup($params['deviceGroup']);
+			if(is_int($deviceGroupId)) {
+				try {
+				SensorDevices::updateDevice($deviceId,'group',$deviceGroupId,$this->db);
+				} catch (\Exception $e) {}
+			}
+		}
+		if(isset($params['deviceParentGroup']) && !empty($params['deviceParentGroup'])) {
+			$deviceGroupParentId = $this->insertDeviceGroup($params['deviceParentGroup']);
+			if(is_int($deviceGroupParentId)) {
+				try {
+					SensorDevices::updateDevice($deviceId, 'group_parent', $deviceGroupParentId, $this->db);
+				} catch (\Exception $e) {
+				}
+			}
+		}
+		if(isset($params['deviceDataTypes']) && is_array($params['deviceDataTypes'])) {
+			foreach($params['deviceDataTypes'] as $array){
+				$dataTypeId = $this->insertDataTypes($array);
+				if(is_int($dataTypeId)) {
+					$this->insertDeviceDataTypes($deviceId,$dataTypeId);
+				}
+			}
+		}
+	}
+
+	/**
+	 * @param int $deviceId
+	 * @param int $dataTypeId
+	 */
+	protected function insertDeviceDataTypes($deviceId,$dataTypeId){
+		$sql = 'INSERT INTO `*PREFIX*sensorlogger_device_data_types` (`user_id`,`device_id`,`data_type_id`) VALUES(?,?,?)';
+		$stmt = $this->db->prepare($sql);
+		$stmt->bindParam(1, $this->userId);
+		$stmt->bindParam(2, $deviceId);
+		$stmt->bindParam(3, $dataTypeId);
+		$stmt->execute();
+	}
+
+	/**
+	 * @param $params
+	 * @return bool;
+	 */
 	protected function checkRegisteredDevice($params) {
 		if(isset($params['deviceId'])) {
 			$deviceId = $params["deviceId"];
@@ -110,11 +237,15 @@ class ApiSensorLoggerController extends ApiController {
 				return true;
 			}
 		}
-		return null;
+		return false;
 	}
 
+	/**
+	 * @param array $array
+	 * @return int|string
+	 */
 	protected function insertDevice($array) {
-		$sql = 'INSERT INTO `*PREFIX*sensorlogger_devices` (`uuid`,`name`,`type`) VALUES(?,?,?)';
+		$sql = 'INSERT INTO `*PREFIX*sensorlogger_devices` (`uuid`,`name`,`type`,`user_id`) VALUES(?,?,?,?)';
 		$stmt = $this->db->prepare($sql);
 
 		if(isset($array['deviceId'])) {
@@ -130,10 +261,14 @@ class ApiSensorLoggerController extends ApiController {
 			$stmt->bindParam(2, $array['deviceName']);
 			//$stmt->bindParam(3, date('Y-m-d H:i:s'));
 			$stmt->bindParam(3, $array['deviceTypeId']);
-			$stmt->execute();
-			return true;
+			$stmt->bindParam(4, $this->userId);
+
+			if($stmt->execute()){
+				return (int)$this->db->lastInsertId();
+			}
 		} else {
 			return 'Missing device ID';
 		}
+		return false;
 	}
 }
