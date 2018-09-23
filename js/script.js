7e2d0a29cc3f72188c9c92761c460f30897227d1
Alexander Stocker <alex@stocker.info>
2018-09-23 11:26:24 +0200

    Added 24hMaxValues widget, Widget Interface, wipeOut Device

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@
 	$(function () {
 		var sidebar = $('#app-sidebar');
 		var saveBtn = $('#save-btn');
+		var wipeOutBtn = $('#wipeout-btn');
 		var showList = $('#showList');
 		var showDashboard = $('#showDashboard');
 		var deviceList = $('#deviceList');
@@ -153,6 +154,7 @@
 		sidebar.on('click','.icon-close',function(e) {
 			sidebar.hide();
 			saveBtn.hide();
+			wipeOutBtn.hide();
 		});
 
 		$(document.body).on('click','.actions',function(e) {
@@ -201,7 +203,7 @@
 				for (var key in response.widgetTypes) {
 					widgetTypeSource.push({
 							value : key,
-							text: response.widgetTypes[key],
+							text: response.widgetTypes[key].displayName,
 							id : key
 						}
 					);
@@ -261,6 +263,7 @@
 
 				sidebarBody.append(widgetType);
 				sidebarBody.append(device);
+                wipeOutBtn.hide();
 
 				sidebarTitle.empty().append('Dashboard widget');
 
@@ -324,13 +327,94 @@
 				return;
 			}
 
-			if(sidebar.is(':visible')) {
-				sidebar.hide();
-				saveBtn.hide();
-				return;
-			}
+            var id = $(this).data('id');
 
-			var id = $(this).data('id');
+            var sidebarFooter = sidebar.find('.footer');
+
+            var wipeOutDevicetest = $('<a/>', {
+                'id':'wipeout',
+                'href': '#',
+                'data-type': 'text',
+                'data-field': 'wipeout',
+                'data-pk': id,
+                'data-url': wipeOutUrl,
+                'data-title': 'response.name',
+                'text': 'wipeOut'
+			});
+
+            //wipeOutDevice.text('wipeOut');
+
+            //console.log(sidebarFooter);
+
+            //sidebarFooter.append(wipeOutDevice);
+
+            if(sidebar.is(':visible')) {
+                sidebar.hide();
+                saveBtn.hide();
+                return;
+            }
+
+            var wipeOutUrl = OC.generateUrl('/apps/sensorlogger/wipeOutDevice');
+
+            var wipeOutDevice = $('#wipeout-btn').show();
+            wipeOutDevice.attr('title','Wipe out device!');
+
+            var wipeOutDeviceConfirm = $('<button/>',{
+                "text": t('sensorlogger', 'Yes')
+            });
+
+            var wipeOutDeviceCancel = $('<button/>',{
+                "text": t('sensorlogger', 'No')
+            });
+
+            $(wipeOutDevice).on('click',function(e){
+                $(".confirm-container").remove();
+            	var confirmContainer = $('<div/>',{
+            		"class": "confirm-container",
+					"text": t('sensorlogger', 'Are you sure?')
+				}).append(wipeOutDeviceConfirm).append(wipeOutDeviceCancel);
+                wipeOutDevice.hide().parent().append(confirmContainer);
+
+                var wipeOutCall = function(id){
+                	var deviceTr = $('table#sensorDevicesTable tr[data-id='+id+']');
+                    var spinner = $('<div class="icon icon-loading">');
+                    confirmContainer.parent().append(spinner);
+                    //console.log(confirmContainer);
+                    confirmContainer.remove();
+                    $.post( wipeOutUrl, {'device_id':id} )
+                        .success(function (response) {
+                        	if(response.success) {
+                                deviceTr.remove();
+                                sidebar.hide();
+                                spinner.remove();
+                                $(".confirm-container").remove();
+                                OC.Notification.showTemporary(t('sensorlogger', 'Wiped out your device completely!'));
+							} else {
+                                wipeOutDevice.show();
+                                $(".confirm-container").remove();
+                                spinner.remove();
+                                OC.Notification.showTemporary(t('sensorlogger', 'Could not wipe out your device!'));
+							}
+                        })
+						.error(function(response) {
+                            wipeOutDevice.show();
+                            $(".confirm-container").remove();
+                            spinner.remove();
+                            OC.Notification.showTemporary(t('sensorlogger', 'Could not wipe out your device!'));
+						});
+                };
+
+
+                wipeOutDeviceCancel.on('click', function(e) {
+                    confirmContainer.remove();
+                    wipeOutDevice.show();
+				});
+                wipeOutDeviceConfirm.on('click', function(e) {
+                    wipeOutCall(id);
+				});
+
+			});
+
 			var url = OC.generateUrl('/apps/sensorlogger/showDeviceDetails/'+id);
 			$.post(url).success(function (response) {
 				$.fn.editable.defaults.mode = 'inline';
@@ -566,7 +650,7 @@
 						if(plotArea.parent().hasClass('widget')) {
 							clonedPlotArea.attr('id', 'chart-' + i).appendTo(plotArea.parent());
 						} else {
-							clonedPlotArea.attr('id', 'chart-' + i).appendTo('#app-content-wrapper .content-wrapper');
+							clonedPlotArea.attr('id', 'chart-' + i).appendTo('#app-content-wrapper');
 						}
 
 					}
