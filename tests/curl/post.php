f31294e4c3e053f67dc50e1c992e99f1f7792385
elex <alex@stocker.info>
2020-12-30 19:02:00 +0100

    Modified test scripts

--- tests/curl/post.php
+++ tests/curl/post.php
@@ -1,21 +1,31 @@
 <?php
-	$url = 'http://owncloud-dev.loc/index.php/apps/sensorlogger/api/v1/createlog/';
+/**
+* POST fake DHT22 sensor data
+*
+* NOTICE: NO device registration required for sensors such as DHT22
+*/
+
+	$host = 'http://localhost:8080'; // Default if https://github.com/sensorlogger/owncloud-docker-development used
+	$path = 'index.php/apps/sensorlogger';
+	$endpoint = 'api/v1/createlog';
+	$url = $host . DIRECTORY_SEPARATOR .
+		$path . DIRECTORY_SEPARATOR .
+		$endpoint . DIRECTORY_SEPARATOR;
 
 	$humidity = mt_rand (1.00*10, 99.99*10) / 10;
 	$temperature = mt_rand (-9.00*10, 49.99*10) / 10;
 
-	$array = array("deviceId" => "101010-fe8c-11e6-bc64-92361f002671",
-					"temperature" => $temperature,
-					"humidity" => $humidity,
-					"date" => date('Y-m-d H:i:s'));
+	$array = [
+	    "deviceId" => "101010-fe8c-11e6-bc64-92361f002671",
+	    "temperature" => $temperature,
+	    "humidity" => $humidity,
+	    "date" => date('Y-m-d H:i:s')
+	];
+
 	$data_json = json_encode($array);
 
 	$username = 'admin';
-<<<<<<< Updated upstream
-	$token = 'MFSFF-VREYH-PHSLT-DTMFN';
-=======
-	$token = 'YPPYJ-WOSGZ-HRUKW-KGSHJ';
->>>>>>> Stashed changes
+	$token = 'TCQDX-RVAMN-TLKFS-ILSSZ';
 
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
@@ -27,4 +37,3 @@
 	$response  = curl_exec($ch);
 	curl_close($ch);
 	return $response;
-?>
